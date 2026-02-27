e2f39fc18cc26427e93688bbf8ba47ccd255458c
Dapa-yap <daffadwin10@gmail.com>
2026-02-27 11:09:12 +0700

    3.4 no 3

--- MINGGU 3/mataKuliahDemo.java
+++ MINGGU 3/mataKuliahDemo.java
@@ -12,10 +12,7 @@ public class mataKuliahDemo {
         }
         for (int i = 0; i < arrayOfMatakuliah.length; i++) {
             System.out.println("Data Mata Kuliah Ke-" + (i+1));
-            System.out.println("Kode    : " + arrayOfMatakuliah[i].kode);
-            System.out.println("Nama    : " + arrayOfMatakuliah[i].nama);
-            System.out.println("SKS     : " + arrayOfMatakuliah[i].sks);
-            System.out.println("Jumlah Jam    : " + arrayOfMatakuliah[i].jumlahJam);
+            arrayOfMatakuliah[i].cetakInfo();
             System.out.println("---------------------------------");
         }
     }
