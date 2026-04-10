012cd46a381e19fa5b7a8b167dc56c83f1061af2
Dapa-yap <daffadwin10@gmail.com>
2026-04-10 14:29:24 +0700

    pertnyaan 5.4.3

--- MINGGU 6/mahasiswaBerprestasi10.java
+++ MINGGU 6/mahasiswaBerprestasi10.java
@@ -43,7 +43,7 @@ public class mahasiswaBerprestasi10 {
         for (int i = 1; i < listMhs.length; i++) {
             mahasiswa10 temp = listMhs[i];
             int j = i;
-            while (j > 0 && listMhs[j - 1].ipk > temp.ipk) {
+            while (j > 0 && listMhs[j - 1].ipk < temp.ipk) {
                 listMhs[j] = listMhs[j - 1];
                 j--;
             }
