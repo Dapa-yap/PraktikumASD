f9b733f23c4579cc2bd8f86399ebf6587bc74e26
Dapa-yap <daffadwin10@gmail.com>
2026-05-10 10:22:25 +0700

    jb9

---
+++ Jobsheet 9/Mahasiswa10.java
@@ -0,0 +1,17 @@
+public class Mahasiswa10 {
+    String nim;
+    String nama;
+    String kelas;
+    int nilai;
+
+    Mahasiswa10(String nim, String nama, String kelas) {
+    this.nama = nama;
+    this.nim = nim;
+    this.kelas = kelas;
+    nilai = -1;
+    }
+    void tugasDinilai(int nilai) {
+    this.nilai = nilai;
+    }
+
+}
