f3cd35a676da47eca5ca1c8755cfc1d6e37c1592
Dapa-yap <daffadwin10@gmail.com>
2026-05-24 15:31:11 +0700

    pertanyaan p2 jb 10

--- P2Jobsheet10/antrianLayanan.java
+++ P2Jobsheet10/antrianLayanan.java
@@ -101,4 +101,13 @@ return mhs;
     public int getJumlahAntrian(){
         return size;
     }
+    public void lihatAkhir() {
+        if (IsEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            System.out.print("Mahasiswa paling belakang: ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[rear].tampilkanData();
+        }
+    }
 }
