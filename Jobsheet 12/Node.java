9c75e88967faecd4ef0c6b9192412f1d1bde5e68
Dapa-yap <daffadwin10@gmail.com>
2026-05-28 11:50:58 +0700

    percobaan 1 jb 12

---
+++ Jobsheet 12/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    Mahasiswa10 data;
+    Node prev;
+    Node next;
+
+    public Node(Mahasiswa10 data){
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
